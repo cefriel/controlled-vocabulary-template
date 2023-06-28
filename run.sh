c389d15d9692e3f56c1030dbdde82995cbf45040
Mario Scrocca <mario.scrocca@gmail.com>
2023-06-28 12:22:00 +0200

    Rename Widoco output

--- run.sh
+++ run.sh
@@ -1,30 +1,36 @@
 #!/bin/bash
 
 RELEASE="$1"
+VOCABULARY="my-vocabulary"
 
 if [ "$RELEASE" = "" ]; then
     echo "Provide a version number x.y.z"
     exit 1
 fi
 
-if [ -d './my-vocabulary/'$RELEASE ]; then
+if [ -d './'$VOCABULARY'/'$RELEASE ]; then
     echo "The provided version already exists." 
 else
 	# From Excel to RDF
-	java -jar tools/xls2rdf.jar convert -i ./my-vocabulary/my-vocabulary.xlsx -o './my-vocabulary/my-vocabulary.ttl' -l en
-	sed -i $'/a skos:ConceptScheme/a\  a owl:Ontology;' ./my-vocabulary/my-vocabulary.ttl
+	java -jar tools/xls2rdf.jar convert -i './'$VOCABULARY'/'$VOCABULARY'.xlsx' -o './'$VOCABULARY'/'$VOCABULARY'.ttl' -l en
+	sed -i $'/a skos:ConceptScheme/a\  a owl:Ontology;' './'$VOCABULARY'/'$VOCABULARY'.ttl'
 
 	sleep 10
 
 	# Clean /latest and create folders
-	rm -r ./my-vocabulary/latest
-	mkdir ./my-vocabulary/latest
-	mkdir './my-vocabulary/'$RELEASE
+	rm -r './'$VOCABULARY'/latest'
+	mkdir './'$VOCABULARY'/latest'
+	mkdir './'$VOCABULARY'/'$RELEASE
 
 	# Create serialisations and documentation
-	java -jar tools/widoco.jar -uniteSections -noPlaceHolderText -ontFile ./my-vocabulary/my-vocabulary.ttl -outFolder ./my-vocabulary/latest -lang en
+	java -jar tools/widoco.jar -uniteSections -noPlaceHolderText -ontFile './'$VOCABULARY'/'$VOCABULARY'.ttl' -outFolder './'$VOCABULARY'/latest' -lang en
 
 	# Move files
-	rm ./my-vocabulary/latest/*.md
-	cp -r ./my-vocabulary/latest/* './my-vocabulary/'$RELEASE
+	mv './'$VOCABULARY'/latest/index-en.html' './'$VOCABULARY'/latest/index.html'
+	mv './'$VOCABULARY'/latest/ontology.jsonld' './'$VOCABULARY'/latest/'$VOCABULARY'.jsonld'
+	mv './'$VOCABULARY'/latest/ontology.nt' './'$VOCABULARY'/latest/'$VOCABULARY'.nt'
+	mv './'$VOCABULARY'/latest/ontology.owl' './'$VOCABULARY'/latest/'$VOCABULARY'.owl'
+	mv './'$VOCABULARY'/latest/ontology.ttl' './'$VOCABULARY'/latest/'$VOCABULARY'.ttl'
+	rm './'$VOCABULARY'/latest/'*'.md'
+	cp -r './'$VOCABULARY'/latest/'* './'$VOCABULARY'/'$RELEASE
 fi
